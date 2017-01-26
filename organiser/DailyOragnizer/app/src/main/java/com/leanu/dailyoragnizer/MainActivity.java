9fffa26d04dc7f7877edab2043358ce3042ccd7a
Mashaev Iulian <polizovateli@mail.ru>
2017-01-26 09:06:01 +0200

    :unamused: added simple notification

--- organiser/DailyOragnizer/app/src/main/java/com/leanu/dailyoragnizer/MainActivity.java
+++ organiser/DailyOragnizer/app/src/main/java/com/leanu/dailyoragnizer/MainActivity.java
@@ -1,6 +1,12 @@
 package com.leanu.dailyoragnizer;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.os.CountDownTimer;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +17,7 @@ public class MainActivity extends ActionBarActivity {
     private long pomodoro_time = 25 * 60 * 1000;//25 minutes in milliseconds
     private long rest_time = 5 * 60 * 1000; //5 minutes in milliseconds
     private boolean isPomodoroStarted = false;
+    private int consecutive_pomodors = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,8 @@ public class MainActivity extends ActionBarActivity {
     public void onStartPomodoro(View view){
         if(isPomodoroStarted) return; //actually I may try to remove button while pomodoro goes:)
         final TextView countdown_display= (TextView)findViewById(R.id.countdown_display);
+        final TextView app_status = (TextView)findViewById(R.id.appStatus);
+        app_status.setText("Time to work");
         new CountDownTimer(pomodoro_time,1000){
             public void onTick(long millisUntilFinished){
                 long minutes = millisUntilFinished/(60*1000);
@@ -29,10 +38,61 @@ public class MainActivity extends ActionBarActivity {
                 countdown_display.setText(String.format("%d:%02d",minutes, seconds));
             }
             public void onFinish() {
-                countdown_display.setText("00:00");
+                notifyPomodoro();
+                finishPomodoro();
             }
         }.start();
         isPomodoroStarted=true;
+    }
+
+    private void finishPomodoro(){
+        //TODO store somewhere pomodoro(by tag/name)
+        //TODO some notification
+        isPomodoroStarted = false;
+        consecutive_pomodors++;
+        int m = 1;//multiplier
+        if(consecutive_pomodors==4) {
+            m = 4;
+            consecutive_pomodors = 0;
+        }
+        final TextView countdown_display= (TextView)findViewById(R.id.countdown_display);
+        final TextView app_status = (TextView)findViewById(R.id.appStatus);
+        app_status.setText("Relax time!");
+        new CountDownTimer(m*rest_time,1000){
+            public void onTick(long millisUntilFinished){
+                long minutes = millisUntilFinished/(60*1000);
+                long seconds = (millisUntilFinished/1000) % 60;
+                countdown_display.setText(String.format("%d:%02d",minutes, seconds));
+            }
+            public void onFinish() {
+                countdown_display.setText("00:00");
+                app_status.setText("Time to work");
+            }
+        }.start();
+
+    }
+
+    private void notifyPomodoro(){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.tomato)
+                .setContentTitle("Pomodoro finished")
+                .setContentText("Have a break")
+                .setAutoCancel(true);
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0,mBuilder.build());
+
     }
 
 
--- organiser/README.md
+++ organiser/README.md
@@ -2,9 +2,10 @@ There should be an pomodoro app.
 
 So far, the following might be the workflow:
  - [x] Create Activity where simple countdown will be shown
+ - [x ] Send notification when pomodoro is finished
  - [ ] Create IntentService for background pomodoro tracking
-   - [ ] Send notifications/start app/somehow notify user(by sound/vibrate/display turn on) that pomodoro is finished
    - [ ] Keep track of time passing and status, when activity will be resumed - the service will be called to check how much time remained
- - [ ] Implement switch of context(Work/Halt/Long Halt)
+ - [x] Implement switch of context(Work/Halt/Long Halt)
  - [ ] May be add tags/history of pomodoros(will need access to persistent memory)
+ - [ ] Add activity where pomodoro time will be customized
  etc.
